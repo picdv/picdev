1247435041c0b6efd8aedd45de0a501c8d032646
gzh <gzhdev@outlook.com>
2022-03-09 17:42:45 +0800

    new url

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-filename = "imgs.txt"  # /*输出目录及文件名*/
+filename = "imgs2.txt"  # /*输出目录及文件名*/
 randimgs = open(filename, "a")
 for numbers in range(1, 1846):  # /*输出序号，这里是从1输出到20*/
     numbers = "%04d" % numbers
-    randimgs.write("https://cdn.jsdelivr.net/gh/gzhstu/picdev/img/" + numbers + ".jpg\n")  # /*图片地址*/
+    randimgs.write("https://raw.ldst.gq/github.com/gzhstu/picdev/blob/main/img/" + numbers + ".jpg\n")  # /*图片地址*/
 randimgs.close()
+# https://raw.ldst.gq/github.com/gzhstu/picdev/blob/main/img/0001.jpg
