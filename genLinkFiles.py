b8e591e15432c6911d4ed9dd44916498f6443d45
picdv <109534783+picdv@users.noreply.github.com>
2022-07-19 01:16:47 +0800

    Create genLinkFiles.py

---
+++ genLinkFiles.py
@@ -0,0 +1,21 @@
+import os
+
+os.chdir("/home/runner/work/picdev/picdev")
+
+
+def genLink(path, linkFilePath):
+    imgNames = os.listdir(path)
+    linkFile = open(linkFilePath, "w")
+    for name in imgNames:  # /*输出序号，这里是从1输出到20*/
+        linkFile.write(f"https://raw.ldst.gq/picdv/picdev/main/{path}/{name}\n")
+        linkFile.close()
+
+
+Path1 = "imgs/web-www"
+filename1 = "www.txt"  # /*输出目录及文件名*/
+Path2 = "imgs/web-ani"
+filename2 = "ani.txt"  # /*输出目录及文件名*/
+
+
+# https://raw.ldst.gq/picdv/picdev/main/imgs/web-ani/001.jpg
+# https://raw.ldst.gq/picdv/picdev/main/imgs/web-www/0006.jpg
