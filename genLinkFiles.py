60c091f68fcc9f6b56de6b48f75fdebec1123e90
picdv <109534783+picdv@users.noreply.github.com>
2022-07-19 01:24:58 +0800

    Update genLinkFiles.py

--- genLinkFiles.py
+++ genLinkFiles.py
@@ -16,6 +16,9 @@ filename1 = "www.txt"  # /*输出目录及文件名*/
 Path2 = "imgs/web-ani"
 filename2 = "ani.txt"  # /*输出目录及文件名*/
 
+genLink(Path1, filename1)
+genLink(Path2, filename2)
+
 
 # https://raw.ldst.gq/picdv/picdev/main/imgs/web-ani/001.jpg
 # https://raw.ldst.gq/picdv/picdev/main/imgs/web-www/0006.jpg
